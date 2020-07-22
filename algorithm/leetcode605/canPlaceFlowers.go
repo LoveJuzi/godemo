ce7e07dd77b2948ac52d7b391b85769d2a7fda21
shengminghui <shengminghui@9fbank.com.cn>
2020-07-22 02:34:21 +0000

    leetcode605

--- algorithm/leetcode605/canPlaceFlowers.go
+++ algorithm/leetcode605/canPlaceFlowers.go
@@ -1,5 +1,46 @@
 package leetcode605
 
+// func canPlaceFlowers(flowerbed []int, n int) bool {
+// 	c := 1
+// 	N := len(flowerbed)
+// 	for i := 0; i < N && n > 0; i++ {
+// 		if flowerbed[i] == 1 {
+// 			c = 0
+// 			continue
+// 		}
+// 		c++
+// 		if c == 3 {
+// 			c = 1
+// 			n--
+// 		}
+// 	}
+
+// 	if n != 0 && c == 2 {
+// 		n--
+// 	}
+
+// 	return n == 0
+// }
+
 func canPlaceFlowers(flowerbed []int, n int) bool {
-	return true
+	c := 1
+	N := len(flowerbed)
+	m := 0
+	for i := 0; i < N; i++ {
+		if flowerbed[i] == 1 {
+			c = 0
+			continue
+		}
+		c++
+		if c == 3 {
+			c = 1
+			m++
+		}
+	}
+
+	if c == 2 {
+		m++
+	}
+
+	return n <= m
 }
--- algorithm/leetcode605/canPlaceFlowers_test.go
+++ algorithm/leetcode605/canPlaceFlowers_test.go
@@ -12,6 +12,14 @@ func Test_canPlaceFlowers(t *testing.T) {
 	}{
 		{[]int{1, 0, 0, 0, 1}, 1, true},
 		{[]int{1, 0, 0, 0, 1}, 2, false},
+		{[]int{1, 0, 0, 0, 1, 0, 0}, 2, true},
+		{[]int{0, 0, 0, 0}, 1, true},
 	}
 
+	for _, c := range cases {
+		result := canPlaceFlowers(c.flowerbed, c.n)
+		if result != c.expected {
+			t.Fatalf("\n canPlaceFlowers: flowerbed is %v, n is %v, expected is %v, result is %v \n", c.flowerbed, c.n, c.expected, result)
+		}
+	}
 }
