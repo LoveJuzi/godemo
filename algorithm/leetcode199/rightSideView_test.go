cf6b9705b858765e3b1199d4474c3b5a86319bd6
shengminghui <shengminghui@9fbank.com.cn>
2020-07-23 09:50:20 +0000

    TreeNode

---
+++ algorithm/leetcode199/rightSideView_test.go
@@ -0,0 +1,7 @@
+package leetcode199
+
+import "testing"
+
+func Test_rightSideView(t *testing.T) {
+	return
+}
--- algorithm/leetcode872/TreeNode.go
+++
@@ -1,8 +0,0 @@
-package leetcode872
-
-// TreeNode 结点定义
-type TreeNode struct {
-	Val   int
-	Left  *TreeNode
-	Right *TreeNode
-}
