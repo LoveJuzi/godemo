23a5142d0c250b8f9977a09a8a580c63416c3071
shengminghui <shengminghui@9fbank.com.cn>
2020-07-31 02:57:26 +0000

    多生产者多消费者代码

--- language/smhMPC/main.go
+++ language/smhMPC/main.go
@@ -4,56 +4,15 @@ import (
 	"fmt"
 )
 
-func main() {
-	<-task(taskMPCMain(3, 10))
-}
-
-// SIZE 生产者消费者通道的大小
-const SIZE int = 1 << 1
-
-func taskMPCMain(m, n int) func() {
-	return func() {
-		MPCMain(m, n)
-	}
-}
-
-// MPCMain 多生产者消费者主程序
-// m 表示生产者的个数，n表示消费者的个数
-func MPCMain(m, n int) {
-	ch := make(chan string, SIZE)
-
-	T1 := []<-chan struct{}{}
-	T2 := []<-chan struct{}{}
-	for i := 0; i < m; i++ {
-		T1 = append(T1, task(taskProducer(ch, i+1)))
-	}
-	for i := 0; i < n; i++ {
-		// 关闭通道，如果消费者产生异常的话，这样生产者也会产生异常
-		T2 = append(T2, task(taskConsumer(ch, i+1), func() { close(ch) }))
-	}
-
-	// 任务控制流
-	for _, v := range T1 { // 等待T1任务结束
-		<-v
-	}
-	close(ch)              // 通知T2任务结束
-	for _, v := range T2 { // 等待T2任务结束
-		<-v
-	}
-}
-
-func task(f func(), rfs ...func()) <-chan struct{} {
-	done := make(chan struct{})
+func bgtask(f func(), ech chan<- struct{}, rfs ...func()) {
 	go func() {
-		defer func() { done <- struct{}{} }() // 退出信号
-		defer recover()                       // 捕获二次产生panic
-		defer func() {                        // 捕获任务的panic
+		defer recover()                      // 捕获一切异常
+		defer func() { ech <- struct{}{} }() // 发送退出信号
+		defer func() {                       // 捕获任务panic
 			if err := recover(); err != nil {
 				// 记录异常日志
-				fmt.Println("======>", err)
+				fmt.Println(err)
 
-				// 执行异常恢复函数
-				// 通常会导致其他相关协程产生异常，而退出
 				for _, v := range rfs {
 					v()
 				}
@@ -62,106 +21,111 @@ func task(f func(), rfs ...func()) <-chan struct{} {
 
 		f()
 	}()
-	return done
 }
 
-func taskProducer(ch chan<- string, id int) func() {
+func synctask(f func(), rfs ...func()) <-chan struct{} {
+	d := make(chan struct{})
+	go func() {
+		defer recover()                    // 捕获一切异常
+		defer func() { d <- struct{}{} }() // 发送退出信号
+		ch := make(chan struct{})          // 后台任务退出信号
+		bgtask(f, ch, rfs...)              // 后台处理任务
+		<-ch                               // 等待后台任务结束
+	}()
+	return d
+}
+
+func main() {
+	<-synctask(taskMPCMain(3, 10))
+}
+
+// SIZE 生产者消费者通道的大小
+const SIZE int = 1 << 1
+
+func taskMPCMain(m, n int) func() {
 	return func() {
-		Producer(ch, id)
+		MPCMain2(m, n)
 	}
 }
 
-func taskConsumer(ch <-chan string, id int) func() {
-	return func() {
-		Consumer(ch, id)
+// MPCMain2 主程序
+func MPCMain2(m, n int) {
+	ch1 := make(chan string, SIZE)
+	ch := make(chan string, SIZE)
+
+	T1 := synctask(taskProducerFactory(ch1, ch, m), func() { close(ch1) }, func() { close(ch) })
+	T2 := synctask(taskConsumerFactory(ch, n), func() { close(ch) })
+
+	for i := 0; i < 1000; i++ { // 任务分发
+		ch1 <- "aaa"
 	}
-}
-
-// Producer 生产者
-func Producer(ch chan<- string, id int) {
-	fmt.Printf("生产者%d开始生产...\n", id)
-	for i := 0; i < 50; i++ {
-		product := fmt.Sprintf("产品%d", i+1)
-		fmt.Printf("生产%d======>%s\n", id, product)
-		ch <- product
-	}
-	fmt.Printf("生产者%d结束生产...\n", id)
-}
-
-// Consumer 消费者
-func Consumer(ch <-chan string, id int) {
-	fmt.Printf("消费者%d开始消费...\n", id)
-	for product := range ch {
-		fmt.Printf("消费%d======>%s\n", id, product)
-		a := 2
-		b := 2
-		fmt.Println(2 / (a - b))
-	}
-	fmt.Printf("消费者%d结束消费...\n", id)
-}
-
-func t1(chs []<-chan string, id int) {
-	ch1 := make(chan string)
-	T1 := task(tasktt1(chs, ch1))
-	T2 := task(taskConsumer(ch1, id))
-
-	<-T1
 	close(ch1)
-	<-T2
+
+	// 任务控制流
+	<-T1      // 等待T1任务结束
+	close(ch) // 通知T2任务结束
+	<-T2      // 等待T2任务结束
 }
 
-func t2(chs []chan<- string, id int) {
-	ch1 := make(chan string)
-
-	T1 := task(taskProducer(ch1, id))
-	T2 := task(tasktt2(chs, ch1))
-
-	<-T1
-	close(ch1)
-	<-T2
-}
-
-func tasktt1(chs []<-chan string, ch1 chan<- string) func() {
+func taskProducerFactory(ch1 <-chan string, ch chan<- string, m int) func() {
 	return func() {
-		tt1(chs, ch1)
+		ProducerFactory(ch1, ch, m)
 	}
 }
 
-// 将多channel数据放入ch1中
-func tt1(chs []<-chan string, ch1 chan<- string) {
-	T := []<-chan struct{}{}
-	for _, ch := range chs {
-		T = append(T, task(taskcopych(ch, ch1)))
+// ProducerFactory 用来启动生产者
+func ProducerFactory(ch1 <-chan string, ch chan<- string, m int) {
+	mch := make(chan struct{}, m) // 生产者个数
+	for i := 0; i < m; i++ {
+		mch <- struct{}{}
 	}
-	for _, v := range T {
-		<-v
+	for v := range ch1 {
+		if _, ok := <-mch; !ok {
+			break
+		}
+		bgtask(taskProducer2(v, ch), mch, func() { close(ch) })
 	}
 }
 
-func tasktt2(chs []chan<- string, ch1 <-chan string) func() {
+func taskProducer2(src string, ch chan<- string) func() {
 	return func() {
-		tt2(chs, ch1)
+		ch <- Producer2(src)
 	}
 }
 
-func tt2(chs []chan<- string, ch1 <-chan string) {
-	T := []<-chan struct{}{}
-	for _, ch := range chs {
-		T = append(T, task(taskcopych(ch1, ch)))
-	}
-	for _, v := range T {
-		<-v
-	}
+// Producer2 生产者加工
+func Producer2(src string) (dst string) {
+	dst = src + "HHHHHHHH"
+	return
 }
 
-func taskcopych(ch <-chan string, ch1 chan<- string) func() {
+func taskConsumerFactory(ch <-chan string, n int) func() {
 	return func() {
-		copych(ch, ch1)
+		ConsumerFactory(ch, n)
 	}
 }
 
-func copych(ch <-chan string, ch1 chan<- string) {
+// ConsumerFactory 用来启动消费者
+func ConsumerFactory(ch <-chan string, n int) {
+	nch := make(chan struct{}, n)
+	for i := 0; i < n; i++ {
+		nch <- struct{}{}
+	}
 	for v := range ch {
-		ch1 <- v
+		if _, ok := <-nch; !ok {
+			break
+		}
+		bgtask(taskConsumer2(v), nch)
 	}
+}
+
+func taskConsumer2(s string) func() {
+	return func() {
+		Consumer2(s)
+	}
+}
+
+// Consumer2 消费者消费
+func Consumer2(s string) {
+	fmt.Println(s)
 }
