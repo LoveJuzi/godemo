8d27b3f2df44396242efb0cd46d9e0ec3b2e5321
LoveJuzi <mylovejuzi@icloud.com>
2026-07-22 22:41:33 +0800

    增加单元测试

--- monkey/evaluator/evaluator_test.go
+++ monkey/evaluator/evaluator_test.go
@@ -1,1 +1,20 @@
 package evaluator
+
+import (
+	"monkey/lexer"
+	"monkey/parser"
+
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEval(t *testing.T) {
+	var input = "15"
+
+	l := lexer.New(input)
+	p := parser.New(l)
+	eval := New(p.ParseProgram())
+	obj := eval.Eval()
+	assert.Equal(t, "15", obj.Inspect())
+}
